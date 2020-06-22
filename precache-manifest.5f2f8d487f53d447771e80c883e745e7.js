2b70dd9a3bb93c08247229b8182b6e0419d52f26
FlexWappa <belford200@ya.ru>
2020-06-22 21:23:16 +0300

    Updates

---
+++ precache-manifest.5f2f8d487f53d447771e80c883e745e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f17991e73f89692b89a4411be18eae3",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "a400b30a282e7f6632f3",
+    "url": "/starwars/static/css/main.38336935.chunk.css"
+  },
+  {
+    "revision": "bdf9fb486b5c5621fe71",
+    "url": "/starwars/static/js/2.5d1dd6ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars/static/js/2.5d1dd6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a400b30a282e7f6632f3",
+    "url": "/starwars/static/js/main.0a21ce2a.chunk.js"
+  },
+  {
+    "revision": "b6c2ad925168875508c9",
+    "url": "/starwars/static/js/runtime-main.19eda298.js"
+  },
+  {
+    "revision": "2c7d2081f91db6d2dd8629eff48bb19a",
+    "url": "/starwars/static/media/big-placeholder.2c7d2081.jpg"
+  },
+  {
+    "revision": "b6af5aeff0ee43a4253fc70c167bb6db",
+    "url": "/starwars/static/media/star-wars-logo-clip-art-1.b6af5aef.png"
+  },
+  {
+    "revision": "b25367452557e69ee9b1c7fde0dbcfab",
+    "url": "/starwars/static/media/starsBackground.b2536745.jpg"
+  }
+]);
